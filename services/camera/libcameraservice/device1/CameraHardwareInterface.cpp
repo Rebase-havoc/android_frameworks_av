9c2644d59994a185b4d16da8f86c957f3c39b943
Abhinav Sohane <asohan@codeaurora.org>
2017-10-18 11:17:18 +0530

    Camera: CameraHardwareInterface: Releasing mHIDLMemoryMapLock in QdataCallback
    Issue: Camera is getting stuck due to mHIDLMemoryMapLock when called for
           QDataCallback then  unregister  memory from HAL.
    Solution: Releasing mHIDLMemoryMapLock after reading mHIDLMemoryMap variable
    
    Change-Id: I7239f6eb6a9dec67393e11b83d0ef6c986215940

--- services/camera/libcameraservice/device1/CameraHardwareInterface.cpp
+++ services/camera/libcameraservice/device1/CameraHardwareInterface.cpp
@@ -140,10 +140,14 @@ hardware::Return<void> CameraHardwareInterface::dataCallback(
 hardware::Return<void> CameraHardwareInterface::QDataCallback(
         DataCallbackMsg msgType, uint32_t data, uint32_t bufferIndex,
         const vendor::qti::hardware::camera::device::V1_0::QCameraFrameMetadata& metadata) {
-    std::lock_guard<std::mutex> lock(mHidlMemPoolMapLock);
-    if (mHidlMemPoolMap.count(data) == 0) {
-        ALOGE("%s: memory pool ID %d not found", __FUNCTION__, data);
-        return hardware::Void();
+    camera_memory_t* mem = nullptr;
+    {
+        std::lock_guard<std::mutex> lock(mHidlMemPoolMapLock);
+        if (mHidlMemPoolMap.count(data) == 0) {
+            ALOGE("%s: memory pool ID %d not found", __FUNCTION__, data);
+            return hardware::Void();
+        }
+        mem = mHidlMemPoolMap.at(data);
     }
     camera_frame_metadata_t md;
     md.number_of_faces = metadata.faces.size();
